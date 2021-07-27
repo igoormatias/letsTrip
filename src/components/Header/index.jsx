87bd25cb1461604f8c0fadbf00c78a77b5141595
Igor Matias <igorhmatias@gmail.com>
2021-07-27 14:48:59 -0300

    feat: adicionando quantidade de reservas

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useSelector} from "react-redux";
 import { Link } from "react-router-dom";
 
 import logo from "../../assets/img/logo.svg";
@@ -6,15 +7,16 @@ import logo from "../../assets/img/logo.svg";
 import  "./styles.css"
 
 export default function Header() {
+  const reserveSize = useSelector(state => state.reserve.length);
   return (
     <header className="container">
       <Link to="/">
         <img src={logo} alt="logo" />
       </Link>
-      <Link className="header__reserva">
+      <Link className="header__reserva" to="/reservas">
         <div>
             <strong>Minhas reservas</strong>
-            <span>0 reservas</span>
+            <span>{reserveSize} reservas</span>
         </div>
       </Link>
     </header>
